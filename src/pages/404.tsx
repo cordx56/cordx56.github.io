2e5c9cf984c427b40092a0d796ff4378e925431d
Chisen Kaoru <cordx56@cordx.net>
2022-10-25 15:59:30 +0900

    404 page title

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,21 +1,23 @@
+import Head from "next/head";
 import Link from "next/link";
 
-const NotFound = () => {
-  return (
-    <div className="main-contents flex justify-center items-center">
-      <main>
-        <div className="flex flec-col mb-2 text-xl">
-          <div className="px-2 border-r-2 border-rose-500">404</div>
-          <div className="px-2">Not Found</div>
-        </div>
-        <ul className="flex justify-center">
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-        </ul>
-      </main>
-    </div>
-  );
-};
+const NotFound = () => (
+  <div className="main-contents flex justify-center items-center">
+    <Head>
+      <title>404 Not found</title>
+    </Head>
+    <main>
+      <div className="flex flec-col mb-2 text-xl">
+        <div className="px-2 border-r-2 border-rose-500">404</div>
+        <div className="px-2">Not Found</div>
+      </div>
+      <ul className="flex justify-center">
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+      </ul>
+    </main>
+  </div>
+);
 
 export default NotFound;
