bb0181ed1e8dea8aacff411cd5926529a52fc731
Chisen Kaoru <cordx56@cordx.net>
2022-10-25 15:56:22 +0900

    modify 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,9 +4,9 @@ const NotFound = () => {
   return (
     <div className="main-contents flex justify-center items-center">
       <main>
-        <div className="flex flec-col mb-2">
-          <div className="px-2 border-r-2 border-rose-500 text-xl">404</div>
-          <div className="px-2 text-xl">Not Found</div>
+        <div className="flex flec-col mb-2 text-xl">
+          <div className="px-2 border-r-2 border-rose-500">404</div>
+          <div className="px-2">Not Found</div>
         </div>
         <ul className="flex justify-center">
           <li>
