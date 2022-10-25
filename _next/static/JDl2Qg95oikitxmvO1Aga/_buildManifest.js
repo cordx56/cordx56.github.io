d4b21f8b7beda2e36c1484c3d5f267301b0c4527
cordx56 <cordx56@users.noreply.github.com>
2022-10-25 14:37:13 +0000

    deploy: 0fdbb320af6a8c1bc50f00f34b90348821c15993

---
+++ _next/static/JDl2Qg95oikitxmvO1Aga/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-941069fb6f449f62.js"],"/404":["static/chunks/pages/404-748b3033487a036e.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/404","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
