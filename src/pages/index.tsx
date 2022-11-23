3a7882c3826eb4676502275855038a5507545012
Chisen Kaoru <cordx56@cordx.net>
2022-11-23 18:33:01 +0900

    add favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,9 @@ const Home = () => (
   <div className="main-contents">
     <Head>
       <title>cordx.cx</title>
+      <link rel="icon" type="image/png" sizes="64x64" href="/favicon.png" />
+      <link rel="icon" type="image/png" sizes="192x192" href="/favicon-large.png" />
+      <link rel="apple-touch-icon" href="/favicon-large.png" />
     </Head>
     <Header />
     <main className="cards-container flex flex-wrap relative z-10">
