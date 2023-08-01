339cb17fc00e84dfab31973e4dbaed193be1c73e
cordx56 <cordx56@users.noreply.github.com>
2023-08-01 20:33:23 +0000

    deploy: dd258c1030f1bfc495bd1f2b8afbf8f8971eab27

--- _next/static/UY8twnsYMDJDX6RYgRuJh/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-f97518e0223aa576.js"],"/404":["static/chunks/pages/404-be1371d81bffcd67.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/404","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
