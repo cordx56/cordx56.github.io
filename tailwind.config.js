8022a93f854e154d5c3681591e4f387379806ba6
CHISEN Kaoru <cordx56@cordx.cx>
2024-01-29 02:48:58 +0900

    update colorcode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       width: {
         192: "48rem",
       },
+      colors: {
+        crimson: {
+          500: "#b12029",
+          700: "#993333"
+        },
+      },
     },
   },
   plugins: [],
-}
+};
