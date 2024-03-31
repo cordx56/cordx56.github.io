c936cab51d9d2d21915489d8d7f09d5cb2b54c0e
CHISEN Kaoru <cordx56@cordx.cx>
2024-04-01 01:05:31 +0900

    修士号

--- src/components/Home/Profile/index.tsx
+++ src/components/Home/Profile/index.tsx
@@ -5,7 +5,7 @@ import Certificate from "./Certificate";
 const Profile = () => (
   <section className="box">
     <h1 className="h1">Profile</h1>
-    <p className="p">Bachelor of Engineering</p>
+    <p className="p">Master of Engineering</p>
     <p className="p">
       Programming Language, Natural Language Processing and Web Service
     </p>
