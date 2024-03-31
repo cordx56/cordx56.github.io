9dac6ee66e71de8bcfb0b5bdf8d9bc05632ba3b4
CHISEN Kaoru <cordx56@cordx.cx>
2024-04-01 01:03:33 +0900

    所属更新

--- src/components/Home/Profile/Affiliation.tsx
+++ src/components/Home/Profile/Affiliation.tsx
@@ -7,15 +7,19 @@ const Affilitaion = () => (
           <td>2017 - 2021</td>
           <td>
             Shibaura Institute of Technology, Information Science and
-            Engineering (Bachelor of Engineering)
+            Engineering
           </td>
         </tr>
         <tr>
-          <td>2021 - Present</td>
+          <td>2021 - 2024</td>
           <td>
             Shibaura Institute of Technology, Electrical Engineering and
             Computer Science
           </td>
+        </tr>
+        <tr>
+          <td>2024 - Present</td>
+          <td>Shibaura Institute of Technology, Functional Control Systems</td>
         </tr>
       </tbody>
     </table>
