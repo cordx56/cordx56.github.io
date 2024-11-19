104bcb38590158c9095a5bc56f4184c526be06fe
CHISEN Kaoru <cordx56@cordx.cx>
2024-11-19 16:36:30 +0900

    migrate to Astro

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
+
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://cordx.cx",
+  integrations: [tailwind(), mdx()],
+});
--- src/components/Home/Footer.tsx
+++
@@ -1,5 +0,0 @@
-const Footer = () => (
-  <div className="py-2 text-center">&copy; 2024 cordx56</div>
-);
-
-export default Footer;
